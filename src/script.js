46c885ddedb234e257b9b4f4f2cd811ccf745c4c
fchyoga <fchyoga@gmail.com>
2024-09-10 19:41:12 +0700

    memisah style.css

---
+++ src/script.js
@@ -0,0 +1,8 @@
+tinymce.init({
+  selector: "textarea.wysiwyg",
+  height: 200,
+  menubar: false,
+  plugins: "lists link image table",
+  toolbar:
+    "undo redo | bold italic | alignleft aligncenter alignright | bullist numlist | link image",
+});
