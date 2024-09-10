3074da3748b8877c285cb2a65142d61d3f58bc11
fchyoga <fchyoga@gmail.com>
2024-09-10 19:17:43 +0700

    halaman detail event ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js}', // Pastikan ini sesuai dengan struktur project Anda
+    "./src/**/*.{html,js}", // Pastikan ini sesuai dengan struktur project Anda
   ],
   theme: {
     extend: {
       colors: {
         red: {
-          DEFAULT: '#94181C', // Default warna merah
+          DEFAULT: "#94181C", // Default warna merah
         },
         neutral: {
-          400: '#94A3B8',
-          500: '#64748B',
-          900: '#0F172A',
+          400: "#94A3B8",
+          500: "#64748B",
+          900: "#0F172A",
         },
       },
       fontFamily: {
-        sans: ['"Plus Jakarta Sans"', 'sans-serif'], // Menambahkan font Plus Jakarta Sans
+        sans: ['"Plus Jakarta Sans"', "sans-serif"], // Menambahkan font Plus Jakarta Sans
       },
     },
   },
